7680dad98f7be70eb7826bf3b468944c3a485f44
AliReza <AliRezaDavodiofficial@yahoo.com>
2022-01-02 16:17:21 +0330

    add hire title

---
+++ src/Components/Hire/Hire.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Hire.scss";
+import HireTitle from "./HireTitle";
+
+const Hire = () => {
+  return (
+    <div className="hire">
+      <HireTitle />
+    </div>
+  );
+};
+
+export default Hire;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,31 +1,12 @@
 import React from "react";
-
-import postImg from "../../assets/Group 2.svg";
-import assignedImg from "../../assets/assign.svg";
-import appliedImg from "../../assets/apply.svg";
+import Hire from "../Hire/Hire";
 
 import "./Main.scss";
 
 const Main = () => {
   return (
     <div className="main">
-      <div className="hire">
-        <h3 className="hire__title"> Hire </h3>
-        <div className="hire__methods">
-          <div className="hire__methods--item">
-            <img src={postImg} alt="post method" />
-            <span> Post </span>
-          </div>
-          <div className="hire__methods--item">
-            <img src={appliedImg} alt="post method" />
-            <span> Applied </span>
-          </div>
-          <div className="hire__methods--item">
-            <img src={assignedImg} alt="post method" />
-            <span> Assigned </span>
-          </div>
-        </div>
-      </div>
+      <Hire />
     </div>
   );
 };
